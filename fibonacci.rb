81632a894e67ee400960b21c28b07beba48c9d82
Sato Junichi <villanova911@gmail.com>
2017-05-30 00:21:59 +0900

    20170530

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
 def fibonacci(num)
-  if num < 2
-    num
-  else
-    a = 0 # n - 2
-    b = 1 # n - 1
-    (num - 2).times do
-      a, b = b, a + b
-    end
-    b
+  return num if num < 2
+  a = 0 # n - 2
+  b = 1 # n - 1
+  2.upto(num) do
+    a, b = b, a + b
   end
+  b
 end
 
 puts "Enter a number to count fibonacci's rabbits."
