7d29807b54bc8cd481af4c18fc97ea3719d8fb9d
Sato Junichi <villanova911@gmail.com>
2017-06-16 17:54:25 +0900

    arrow functions

---
+++ javascript/chapter6/arrowFunctions.js
@@ -0,0 +1,7 @@
+const f1 = () => 'Hello!';
+const f2 = name => `Hello, ${name}.`;
+const f3 = (a, b) => a + b;
+
+console.log(f1());       // Hello!
+console.log(f2('John')); // Hello, John.
+console.log(f3(10, 20)); // 30
--- javascript/chapter6/rememberThis.js
+++ javascript/chapter6/rememberThis.js
@@ -12,5 +12,20 @@ const julie = {
     return getReverse();
   }
 };
-
 console.log(julie.greetBackwards()); // eiluJ
+
+// 'this' is bind lexically to receiver object, namely stephen.
+const stephen = {
+  name: 'Stephen',
+  greetBackwards: function() {
+    const getReverse = () => {
+      let nameBackwards = '';
+      for (let i = this.name.length - 1; i >= 0; i--) {
+        nameBackwards += this.name[i];
+      }
+      return nameBackwards;
+    }
+    return getReverse();
+  }
+}
+console.log(stephen.greetBackwards()); // nehpetS
